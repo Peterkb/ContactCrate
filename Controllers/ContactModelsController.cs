c04e9e4c019e9763c60cf43624bbcf8b746516cf
Peter <peterbredell@gmail.com>
2022-09-22 18:10:56 +0200

    commit

---
+++ Controllers/ContactModelsController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ContactCrate.Data;
+using ContactCrate.Models;
+
+namespace ContactCrate.Controllers
+{
+    public class ContactModelsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ContactModelsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ContactModels
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.Contacts.ToListAsync());
+        }
+
+        // GET: ContactModels/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Contacts == null)
+            {
+                return NotFound();
+            }
+
+            var contactModel = await _context.Contacts
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (contactModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(contactModel);
+        }
+
+        // GET: ContactModels/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ContactModels/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,AppUserID,FirstName,LastName,BirthDate,Address1,Address2,City,State,Country,ZipCode,Email,LandlineNumber,CellNumber,DateCreated,ImageData,ImageType")] ContactModel contactModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(contactModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(contactModel);
+        }
+
+        // GET: ContactModels/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Contacts == null)
+            {
+                return NotFound();
+            }
+
+            var contactModel = await _context.Contacts.FindAsync(id);
+            if (contactModel == null)
+            {
+                return NotFound();
+            }
+            return View(contactModel);
+        }
+
+        // POST: ContactModels/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,AppUserID,FirstName,LastName,BirthDate,Address1,Address2,City,State,Country,ZipCode,Email,LandlineNumber,CellNumber,DateCreated,ImageData,ImageType")] ContactModel contactModel)
+        {
+            if (id != contactModel.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(contactModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ContactModelExists(contactModel.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(contactModel);
+        }
+
+        // GET: ContactModels/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Contacts == null)
+            {
+                return NotFound();
+            }
+
+            var contactModel = await _context.Contacts
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (contactModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(contactModel);
+        }
+
+        // POST: ContactModels/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Contacts == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Contacts'  is null.");
+            }
+            var contactModel = await _context.Contacts.FindAsync(id);
+            if (contactModel != null)
+            {
+                _context.Contacts.Remove(contactModel);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ContactModelExists(int id)
+        {
+          return _context.Contacts.Any(e => e.Id == id);
+        }
+    }
+}
